c02238ced42cec6e71d2fd2f75a91892af27c1da
davarezza <dapva2728@gmail.com>
2024-03-18 21:41:18 +0700

    feat: update dashboard view

--- resources/views/admin/page/dashboard.blade.php
+++ resources/views/admin/page/dashboard.blade.php
@@ -2,71 +2,71 @@
 
 @section('container')
     <div class="container-fluid">
-        <div class="row">
+        <div class="row pb-3">
             <div class="col-md-11 offset-md-1">
-                <div class="count-data">
-                    <div>
+                <div class="row gy-4">
+                    <div class="col-md-3 mb-2">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-tachometer-alt"></i> Pengumumman</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-bullhorn"></i>Agenda</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-newspaper"></i>Berita</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-book"></i>Artikel</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3 mb-2">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-images"></i>Gallery</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-school"></i>Extrakurikuler</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-laptop"></i>Fasilitas</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-users"></i>Peserta didik</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-user-tie"></i>PTK</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
-                    <div>
+                    <div class="col-md-3">
                         <div class="count-card rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-link"></i>Links</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span class="font-weight-bold"></span> Item</p>
                         </div>
                     </div>
                 </div>
-            </div>
+            {{-- </div> --}}
         </div>
     </div>
 @endsection
