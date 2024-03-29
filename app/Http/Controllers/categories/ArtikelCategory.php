e929e826de028432d98068f4ff71954fe8139c53
davarezza <dapva2728@gmail.com>
2024-03-29 20:23:37 +0700

    feat: adding CRUD for article category

--- app/Http/Controllers/categories/ArtikelCategory.php
+++ app/Http/Controllers/categories/ArtikelCategory.php
@@ -3,25 +3,108 @@
 namespace App\Http\Controllers\categories;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\tb_category_artikel;
+use Illuminate\Http\Request;
 
 class ArtikelCategory extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(Request $request)
     {
         $token = $request->session()->get('token') ?? $request->input('token');
-        $artikel = tb_category_artikel::all();
+        $articles = tb_category_artikel::all();
         $action = $_GET['action'] ?? '';
 //        return $request->session()->all();
 
-        return view('admin.categories.beritacategory.index', [
+        return view('admin.categories.artikelcategory.index', [
             'menu_active' => 'berita',
             'action' => $action,
-            'artikel' => $artikel,
+            'articles' => $articles,
             'token' => $token,
             "category" => $request->session()->get("category") ?? null,
             "action" => $request->session()->get("update") ?? false,
         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $token = $request->session()->get('token') ?? $request->input('token');
+        $action = 'create';
+        $data = [
+            'update' => $action,
+        ];
+        return redirect()->route("artikel.category.index",$token)->with($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required',
+        ]);
+
+        // Simpan data kategori
+        tb_category_artikel::create([
+            'category_name' => $request->category_name,
+        ]);
+
+        return redirect()->route('artikel.category.index', ['token' => $request->token])->with('success', 'Kategori berhasil ditambahkan.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Request $request)
+    {
+        $id_category = $request->route("artikel_category");
+        $token = $request->session()->get('token') ?? $request->input('token');
+        $action = 'update';
+        $request->session()->put('token',$token);
+        $category = tb_category_artikel::findOrFail($id_category);
+        $data = [
+            'category' => $category,
+            'update' => $action,
+        ];
+        return redirect()->route("artikel.category.index",$token)->with($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required',
+        ]);
+        $artikel_category = $request->route("artikel_category");
+
+        // Update data category
+        $category = tb_category_artikel::findOrFail($artikel_category);
+        $category->update([
+            'category_name' => $request->category_name,
+        ]);
+
+        return redirect()->route('artikel.category.index', ['token' => $request->token])->with('success', 'Kategori berhasil diperbarui.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        $id_category = $request->route("artikel_category");
+        $token = $request->session()->get('token') ?? $request->input('token');
+
+       $category = tb_category_artikel::findOrFail($id_category);
+       $category->delete();
+
+        return redirect()->route('artikel.category.index', ['token' => $token])->with('success', 'Kategori berhasil dihapus.');
     }
 }
--- resources/views/admin/categories/beritacategory/index.blade.php
+++ resources/views/admin/categories/beritacategory/index.blade.php
@@ -28,7 +28,7 @@
                         </div>
                         <div class="form-group">
                             <label for="nama" class="mt-3 mb-2">Nama Kategori</label>
-                            <input type="text" value="{{$category->category_name}}" name="category_name" id="category_name" class="form-control" placeholder="Perayaan / Peristiwa" aria-describedby="namaID">
+                            <input type="text" required value="{{$category->category_name}}" name="category_name" id="category_name" class="form-control" placeholder="Perayaan / Peristiwa" aria-describedby="namaID">
                             <small id="namaID" class="text-muted d-none">Nama</small>
                         </div>
                         <div class="text-right w-100 position-absolute" style="right: 10px;">
