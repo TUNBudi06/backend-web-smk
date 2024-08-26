20e69c47a29134cafcab6b429e18e4451abc7b7e
TUNBudi06 <anjaytun2006@gmail.com>
2024-08-26 11:21:05 +0700

    Restrict dashboard sections to Super Admin
    Added conditional checks to display certain dashboard sections only to users with a role of Super Admin. Also modified the background color of these sections to differentiate them visually.

--- resources/views/admin/page/dashboard.blade.php
+++ resources/views/admin/page/dashboard.blade.php
@@ -36,14 +36,16 @@
                                         class="font-weight-bold">{{$artikel}}</span> Item</p>
                         </div>
                     </div>
-                    <div class="col-md-12 mb-1">
-                        <div class="count-comment bg-gray rounded p-3 text-center">
-                            <p class="poppins mb-0"><i class="fas mr-2 fa-user"></i>Super Admin only</p>
+                    @if(session()->get('user')->role == 1)
+                        <div class="col-md-12 mb-1">
+                            <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3 text-center">
+                                <p class="poppins mb-0"><i class="fas mr-2 fa-user"></i>Super Admin only</p>
+                            </div>
                         </div>
-                    </div>
+                    @endif
                     <div class="col-md-6 mb-1"
                          @if(session()->get('user')->role == 1) onclick="window.location.href='{{ route('jurusan.index', ['token' => $token]) }}';" @endif>
-                        <div class="count-comment bg-gray rounded p-3">
+                        <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-pen-nib"></i>Jurusan</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;">{{ $jurusan }}<span
                                         class="font-weight-bold"></span> Item</p>
@@ -51,7 +53,7 @@
                     </div>
                     <div class="col-md-6 mb-1"
                          @if(session()->get('user')->role == 1) onclick="window.location.href='{{ route('extra.index', ['token' => $token]) }}';" @endif>
-                        <div class="count-comment bg-gray rounded p-3">
+                        <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-school"></i>Extrakurikuler</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span
                                         class="font-weight-bold">{{$extra}}</span> Item</p>
@@ -59,7 +61,7 @@
                     </div>
                     <div class="col-md-6 mb-1"
                          @if(session()->get('user')->role == 1) onclick="window.location.href='{{ route('fasilitas.index', ['token' => $token]) }}';" @endif>
-                        <div class="count-comment bg-gray rounded p-3">
+                        <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-laptop"></i>Fasilitas</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span
                                         class="font-weight-bold">{{ $fasilitas }}</span> Item</p>
@@ -67,7 +69,7 @@
                     </div>
                     <div class="col-md-6 mb-1"
                          @if(session()->get('user')->role == 1) onclick="window.location.href='{{ route('pd.index', ['token' => $token]) }}';" @endif>
-                        <div class="count-comment bg-gray rounded p-3">
+                        <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-users"></i>Peserta didik</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span
                                         class="font-weight-bold">{{ $pd }}</span> Item</p>
@@ -75,7 +77,7 @@
                     </div>
                     <div class="col-md-6 mb-1"
                          @if(session()->get('user')->role == 1) onclick="window.location.href='{{ route('ptk.index', ['token' => $token]) }}';" @endif>
-                        <div class="count-comment bg-gray rounded p-3">
+                        <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-user-tie"></i>PTK</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span
                                         class="font-weight-bold">{{ $ptk }}</span> Item</p>
@@ -83,7 +85,7 @@
                     </div>
                     <div class="col-md-6 mb-1"
                          @if(session()->get('user')->role == 1) onclick="window.location.href='{{ route('gallery.index', ['token' => $token]) }}';" @endif>
-                        <div class="count-comment bg-gray rounded p-3">
+                        <div class="count-comment {{session()->get('user')->role == 1 ? 'bg-warning' : 'bg-gray'}} rounded p-3">
                             <p class="poppins mb-0"><i class="fas mr-2 fa-images"></i>Gallery</p>
                             <p class="montserrat mb-0" style="font-size: .8rem;"><span
                                         class="font-weight-bold">{{$gallery}}</span> Item</p>
