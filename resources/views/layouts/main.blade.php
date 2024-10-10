ce8072a2837577424250a7824c9b99fd916166d4
TUNBudi06 <anjaytun2006@gmail.com>
2024-10-10 10:38:34 +0700

    keknya emang broken stylenya "saya gak bisa fix"

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,7 +34,7 @@
         @yield('container')
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 {{--    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>--}}
     <script src="{{ asset('js/all.min.js') }}"></script>
     <script src="{{ asset('js/sharer.min.js') }}"></script>
