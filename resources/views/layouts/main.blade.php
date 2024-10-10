476b5864919992d4218205a8fa49e79c30879f5f
TUNBudi06 <anjaytun2006@gmail.com>
2024-10-10 10:12:56 +0700

    generate new datatables for bs4 since likely using bs5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,9 +14,11 @@
     <link rel="stylesheet" href="{{ asset('css/jssocials-theme-flat.css') }}">
     <link rel="stylesheet" href="{{ asset('css/jssocials-theme-minima.css') }}">
     <link rel="icon" type="image/x-icon" class="rounded-circle" href="{{ asset('smkicon.ico') }}" />
-    <link href="https://cdn.datatables.net/v/bs5/dt-2.1.7/datatables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/v/bs4/jszip-3.10.1/dt-2.1.8/af-2.7.0/b-3.1.2/b-colvis-3.1.2/b-html5-3.1.2/b-print-3.1.2/fc-5.0.3/fh-4.0.1/r-3.0.3/datatables.min.css" rel="stylesheet">
 
-    <script src="https://cdn.datatables.net/v/bs5/dt-2.1.7/datatables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/v/bs4/jszip-3.10.1/dt-2.1.8/af-2.7.0/b-3.1.2/b-colvis-3.1.2/b-html5-3.1.2/b-print-3.1.2/fc-5.0.3/fh-4.0.1/r-3.0.3/datatables.min.js"></script>
     @if ($menu_active)
         <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
     @endif
